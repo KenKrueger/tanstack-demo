bad0dae96dcd8919d0287e4da271e1d4383aa304
Ken <12816468+KenKrueger@users.noreply.github.com>
2025-02-02 15:52:15 -0600

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({}),
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
