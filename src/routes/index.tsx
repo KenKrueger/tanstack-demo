b8bd6556418892bf7ef52f1c29eeb1853fd9ea0a
Ken <12816468+KenKrueger@users.noreply.github.com>
2025-02-03 20:21:08 -0600

    hover on account

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -64,7 +64,7 @@ interface AccountCardProps extends LinkProps {
 function AccountCard({ title, children, to, params }: AccountCardProps) {
   return (
     <WrappedLink to={to} params={params}>
-      <Card>
+      <Card className="hover:bg-gray-100">
         <div className="font-semibold text-xl">{title}</div>
         <div>{children}</div>
       </Card>
