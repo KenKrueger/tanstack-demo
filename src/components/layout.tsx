c872540ce8419f5a63d5ebf6c8cf7ae46236ddb0
Ken <12816468+KenKrueger@users.noreply.github.com>
2025-02-05 00:03:14 -0600

    fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -73,10 +73,10 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
           <span className="sr-only">Back</span>
         </Button>
       )}
-      <main className="pb-[calc(4rem+env(safe-area-inset-bottom))]">
+      <main className="pb-[calc(4rem+env(safe-area-inset-bottom))] bg-zinc-50">
         {children}
       </main>
-      <nav className="fixed bottom-0 left-0 right-0 z-50 flex justify-around bg-[#F5F5F5]/95 backdrop-blur-lg p-1 border-t border-gray-300 pb-[env(safe-area-inset-bottom)]">
+      <nav className="fixed bottom-0 left-0 right-0 z-50 flex justify-around bg-zinc-100 backdrop-blur-lg p-1 border-t border-gray-300 pb-[env(safe-area-inset-bottom)]">
         <MyNavLink to="/" icon={HomeIcon} label="Home" />
         <MyNavLink
           to="/move-money"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -28,7 +28,7 @@ export const Route = createFileRoute("/")({
 function IndexPage() {
   return (
     <div className="min-h-screen ">
-      <header className="p-4 bg-white shadow-sm">
+      <header className="p-4">
         <h1 className="text-4xl font-semibold text-zinc-800">Home</h1>
       </header>
       <main className="max-w-4xl mx-auto p-4 space-y-8">
